edfa0e323349bca613b09e4236b95201906139b9
Richard <hello@noxan.net>
2017-03-13 17:19:28 +0100

    rewrite index.js to import same files again but use npm instead of bower

--- index.js
+++ index.js
@@ -2,5 +2,24 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-file-saver'
+  name: 'ember-cli-file-saver',
+
+  included(app) {
+    this._super.included(app);
+
+    app.import(this.treePaths.vendor + '/file-saver/FileSaver.js', {
+      type: 'vendor'
+    });
+
+    app.import('vendor/jquery-ajax-arraybuffer.js', {
+      type: 'vendor'
+    });
+
+    app.import('vendor/file-saver.shim.js', {
+      type: 'vendor',
+      exports: {
+        rison: ['default']
+      }
+    });
+  }
 };
