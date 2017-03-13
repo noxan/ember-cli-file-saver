83a945ff98c7ffd7aa6829141f087eee16610787
Richard <hello@noxan.net>
2017-03-13 17:07:50 +0100

    upgrade to latest ember version

--- index.js
+++ index.js
@@ -2,24 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-file-saver',
-
-  included(app) {
-    this._super.included(app);
-
-    app.import(app.bowerDirectory + '/FileSaver/FileSaver.js', {
-      type: 'vendor'
-    });
-
-    app.import('vendor/jquery-ajax-arraybuffer.js', {
-      type: 'vendor'
-    });
-
-    app.import('vendor/file-saver.shim.js', {
-      type: 'vendor',
-      exports: {
-        rison: ['default']
-      }
-    });
-  }
+  name: 'ember-cli-file-saver'
 };
